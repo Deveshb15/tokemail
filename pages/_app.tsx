682f771a3a51e82731ea7368e5d63a4efd671363
Deveshb15 <deveshb583@gmail.com>
2024-06-23 14:12:44 +0530

    add privy provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,29 +2,26 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/components/ui/themeProvider";
 import { Space_Mono, Inter } from "next/font/google";
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
 import {
   getDefaultConfig,
   RainbowKitProvider,
-  darkTheme
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import {
-  base, baseSepolia,
-} from 'wagmi/chains';
-import {
-  QueryClientProvider,
-  QueryClient,
-} from "@tanstack/react-query";
-import { Toaster } from 'react-hot-toast';
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { base, baseSepolia } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 import { defineChain } from "viem";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
 import Layout from "@/components/shared/Layout";
-import splitbee from '@splitbee/web';
+import splitbee from "@splitbee/web";
+
+import {PrivyProvider} from '@privy-io/react-auth';
 
 const config = getDefaultConfig({
   appName: "HigherLINK",
-  projectId: '7febbd905df720d5866a44b58cd1b1a9',
+  projectId: "7febbd905df720d5866a44b58cd1b1a9",
   chains: [base, baseSepolia],
 });
 const queryClient = new QueryClient();
@@ -35,35 +32,40 @@ const spaceMono = Space_Mono({
   variable: "--font-space-mono",
 });
 
-
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 const rainbowKitTheme = darkTheme({
-  accentColor: '#9261E1',
-  accentColorForeground: 'white',
-  borderRadius: 'medium',
-  fontStack: 'system',
-  overlayBlur: 'large',
-})
+  accentColor: "#9261E1",
+  accentColorForeground: "white",
+  borderRadius: "medium",
+  fontStack: "system",
+  overlayBlur: "large",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  splitbee.init()
+  splitbee.init();
   return (
     <div className={`${spaceMono.variable} ${inter.variable}`}>
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider theme={rainbowKitTheme}>
-            <ThemeProvider attribute="class" defaultTheme="dark">
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-              <GoogleAnalytics gaId="G-EBMRHG4GXX" />
-              <Toaster toastOptions={{
-                position: "top-right",
-                className: `${inter.className}`,
-              }} />
-            </ThemeProvider>
-          </RainbowKitProvider>
+          <PrivyProvider
+            appId="clxr9cygi06gn3v696ck1f0ma"
+          >
+            <RainbowKitProvider theme={rainbowKitTheme}>
+              <ThemeProvider attribute="class" defaultTheme="dark">
+                <Layout>
+                  <Component {...pageProps} />
+                </Layout>
+                <GoogleAnalytics gaId="G-EBMRHG4GXX" />
+                <Toaster
+                  toastOptions={{
+                    position: "top-right",
+                    className: `${inter.className}`,
+                  }}
+                />
+              </ThemeProvider>
+            </RainbowKitProvider>
+          </PrivyProvider>
         </QueryClientProvider>
       </WagmiProvider>
     </div>
